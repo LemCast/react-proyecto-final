63080f1f497b3cf4491ebfd0538bc2595d0c814c
Aaron Garza <aaron.garza500@gmail.com>
2026-09-04 00:35:03 -0600

    Once again fixed a vite config revert error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'proyecto-react-final'
+const repoName = 'react-proyecto-final'
 const isGitHubPages = process.env.GITHUB_PAGES === 'true'
 
 export default defineConfig({
